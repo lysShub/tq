9a5e827be6bfd0bea3dfcbbe25c1a714a8065956
lysShub <2454148255@qq.com>
2021-04-29 09:38:50 +0800

    lysShub

--- test/test.go
+++ test/test.go
@@ -14,26 +14,25 @@ func main() {
 
 	var st time.Time
 
-	st = time.Now()
 	go func() {
 		var r interface{}
 		for {
 			r = <-(Q.MQ)
 			v, ok := r.(string)
 			if ok {
-				fmt.Println(v, " 实际延时:", time.Now().Sub(st))
+				go fmt.Println(v, " 实际延时:", time.Now().Sub(st))
 			} else {
 				fmt.Println("类型错误")
 			}
 		}
 	}()
 
+	st = time.Now()
 	for i := 0; i < 20; i++ {
 		go Q.Add(tq.Ts{ // 并发安全
 			T: time.Now().Add(time.Second * time.Duration(i)),
 			P: "设定延时:" + strconv.Itoa(i) + "s",
 		})
-
 	}
 
 	time.Sleep(time.Second * 25)
--- tq.go
+++ tq.go
@@ -16,36 +16,38 @@ type TQ struct {
 	MQ chan interface{}
 
 	/* 内部 */
-	chans map[int64](chan Ts) // 储存任务
-	ends  map[int64]time.Time // 记录对应管道的最后一次任务的时间
-	imr   chan Ts             //
-	dcl   int                 // 默认任务管道容量
-	cid   chan int64          // 传递id，表示新建了管道
-	wc    sync.Mutex          // 读写锁
+
+	transChans     map[int64](chan Ts) // 储存任务
+	endTimes       map[int64]time.Time // 记录对应管道的最后任务的执行时间
+	addChan        chan Ts             // 增加任务
+	idsChan        chan int64          // 传递id，表示新建了管道
+	lock           sync.Mutex          // 读写锁
+	defaultChanLen int                 // 默认任务管道容量
+
 }
 
 // Ts 表示一个任务
 type Ts struct {
 	T time.Time   // 预定执行UTC时间
-	P interface{} // 执行时MQ返回的数据
+	P interface{} // 执行时返回的数据
 }
 
 // Run 启动
 func (t *TQ) Run() {
 
-	t.imr = make(chan Ts, 64)
-	t.cid = make(chan int64, 16)
+	t.addChan = make(chan Ts, 64)
+	t.idsChan = make(chan int64, 16)
 	t.MQ = make(chan interface{}, 64)
-	t.chans = make(map[int64](chan Ts))
-	t.ends = make(map[int64]time.Time)
-	t.dcl = 64
+	t.transChans = make(map[int64](chan Ts))
+	t.endTimes = make(map[int64]time.Time)
+	t.defaultChanLen = 64
 
 	// 执行任务
 	go func() {
 		for { // 新建了管道
 			select {
-			case i := <-t.cid:
-				go t.exec(i)
+			case id := <-t.idsChan:
+				go t.exec(t.transChans[id], id)
 			case <-time.After(time.Minute):
 				// nothing
 			}
@@ -57,38 +59,38 @@ func (t *TQ) Run() {
 		var r Ts
 		for {
 			select {
-			case r = <-t.imr:
+			case r = <-t.addChan:
 
-				if len(t.ends) == 0 { // 第一次
+				if len(t.endTimes) == 0 { // 第一次
 
-					var sc chan Ts = make(chan Ts, t.dcl*2)
-					var id = t.randId()
+					var sc chan Ts = make(chan Ts, t.defaultChanLen*2)
+					var id = int64(0) // 此资源不会被释放
 
-					t.chans[id] = sc
-					t.ends[id] = r.T
-					t.chans[id] <- r
-					t.cid <- id
+					t.transChans[id] = sc
+					t.endTimes[id] = r.T
+					t.transChans[id] <- r
+					t.idsChan <- id
 				} else {
 					var flag bool = false
-					for id, v := range t.ends {
+					for id, v := range t.endTimes {
 
-						if r.T.After(v) && len(t.chans[id]) < t.dcl { //追加
+						if r.T.After(v) && len(t.transChans[id]) < t.defaultChanLen { //追加
 
-							t.chans[id] <- r
-							t.ends[id] = r.T
+							t.transChans[id] <- r
+							t.endTimes[id] = r.T
 							flag = true
 							break
 						}
 					}
 					// 需要新建管道
 					if !flag {
-						var sc chan Ts = make(chan Ts, t.dcl)
+						var sc chan Ts = make(chan Ts, t.defaultChanLen)
 						var id = t.randId()
 
-						t.chans[id] = sc
-						t.ends[id] = r.T
-						t.chans[id] <- r
-						t.cid <- id
+						t.transChans[id] = sc
+						t.endTimes[id] = r.T
+						t.transChans[id] <- r
+						t.idsChan <- id
 					}
 				}
 
@@ -99,39 +101,36 @@ func (t *TQ) Run() {
 		}
 	}()
 
-	time.Sleep(time.Millisecond * 20)
+	// time.Sleep(time.Millisecond * 20)
 }
 
 // Add 增加任务
 func (t *TQ) Add(r Ts) error {
-	if cap(t.imr)-len(t.imr) < 1 {
-		return errors.New("channel block! len:" + strconv.Itoa(len(t.imr)) + " ,cap:" + strconv.Itoa(cap(t.imr)))
+	if cap(t.addChan)-len(t.addChan) < 1 {
+		return errors.New("channel block! len:" + strconv.Itoa(len(t.addChan)) + " ,cap:" + strconv.Itoa(cap(t.addChan)))
 	}
-	t.imr <- r
+	t.addChan <- r
 	return nil
 }
 
 // exec 执行任务
-func (t *TQ) exec(id int64) {
+func (t *TQ) exec(c chan Ts, id int64) {
 	var ts Ts
-
 	for {
 
-		t.wc.Lock()
-		// 执行完任务后应该退出
-		if len(t.chans[id]) == 0 {
-
-			delete(t.ends, id)  // 删除ends中记录
-			close(t.chans[id])  // 关闭管道
-			delete(t.chans, id) // 删除chans中记录
-
-			t.wc.Unlock()
+		t.lock.Lock()
+		// 执行完任务后释放资源
+		if id != 0 && len(c) == 0 {
+			delete(t.endTimes, id)   // 删除ends中记录
+			close(c)                 // 关闭管道
+			delete(t.transChans, id) // 删除chans中记录
+			t.lock.Unlock()
 			return
 		}
-		t.wc.Unlock()
+		t.lock.Unlock()
 
-		ts = <-t.chans[id]
-		time.Sleep(ts.T.Sub(time.Now())) //延时
+		ts = <-c
+		time.Sleep(time.Until(ts.T)) //延时
 
 		t.MQ <- ts.P
 	}
